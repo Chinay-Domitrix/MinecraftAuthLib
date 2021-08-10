7cb0491b0638484f126dc8bc8ebec34ec829da86
Chinay-Domitrix <55807470+Chinay-Domitrix@users.noreply.github.com>
2021-08-10 17:08:35 -0400

    Update MinecraftAuthLib.java

--- src/main/java/net/badbird5907/authlib/MinecraftAuthLib.java
+++ src/main/java/net/badbird5907/authlib/MinecraftAuthLib.java
@@ -4,7 +4,8 @@ import net.badbird5907.authlib.handlers.UserPassHandler;
 import net.badbird5907.authlib.object.AuthResponse;
 
 public class MinecraftAuthLib {
-	public static String CLIENT_ID = "", CLIENT_SECRET = "", REDIRECT_URI = "https://redirect.auth.badbird5907.net"; //redirect isn't used
+	public static String CLIENT_ID = "", CLIENT_SECRET = "", REDIRECT_URI = "https://redirect.auth.badbird5907.net"; // redirect isn't used
+
 	public static void setup(String client, String secret) {
 		CLIENT_ID = client;
 		CLIENT_SECRET = secret;
